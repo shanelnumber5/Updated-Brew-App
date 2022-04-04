4d88c36092f2640b580ba9c0e13111d5d52a3528
Shanel Locke <77364986+shanelnumber5@users.noreply.github.com>
2022-04-03 18:15:15 -0600

    Gross update...formatting!

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -17,17 +17,6 @@
  * under the License.
  */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
